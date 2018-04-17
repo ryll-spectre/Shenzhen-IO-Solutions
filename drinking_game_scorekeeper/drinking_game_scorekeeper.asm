e1089556de7bb43f3da6fdb5b2c7cbea68b159e3
jjgccg <joseph.george@richmond.edu>
2018-04-16 20:38:58 -0400

    two new problems added

---
+++ drinking_game_scorekeeper/drinking_game_scorekeeper.asm
@@ -0,0 +1,9 @@
+  teq p1 100
++ add 1
+  teq p0 100
++ sub 2
+#acc < 0 ?
+  tlt acc 0
++ mov 0 acc
+  mov acc x1
+  slp 1
---
+++ harmonic_maximization_engine/harmonic_maximization_engine.asm
@@ -0,0 +1,12 @@
+#Top MC4000
+  mov x1 acc
+  tgt p0 0
++ sub 50
++ mul 4
++ add 50
+  mov acc p1
+  slp 1
+
+#Bottom 
+  mov p0 x0
+  slp 1
